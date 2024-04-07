ad3a3362a69c1efed3bd7c80bea152c3c7098096
Jorge Macana <39782680+andresmacana@users.noreply.github.com>
2024-04-07 11:48:05 -0400

    c5

--- src/IntraB.js
+++ src/IntraB.js
@@ -6,17 +6,17 @@ import katex from "katex";
 import "katex/dist/katex.min.css";
 
 const CalculationsForm2 = () => {
-  const [mA, setmA] = useState("");
-  const [kV, setkV] = useState("");
-  const [distance, setdistance] = useState("");
-  const [distanceSkin, setdistanceSkin] = useState("");
-  const [time1, settime1] = useState("");
+  const [mA, setmA] = useState("6");
+  const [kV, setkV] = useState("70");
+  const [distance, setdistance] = useState("30");
+  const [distanceSkin, setdistanceSkin] = useState("20");
+  const [time1, settime1] = useState("0.1");
   const [exposure1, setexposure1] = useState("");
-  const [time2, settime2] = useState("");
+  const [time2, settime2] = useState("0.16");
   const [exposure2, setexposure2] = useState("");
-  const [time3, settime3] = useState("");
+  const [time3, settime3] = useState("0.20");
   const [exposure3, setexposure3] = useState("");
-  const [time4, settime4] = useState("");
+  const [time4, settime4] = useState("0.32");
   const [exposure4, setexposure4] = useState("");
   const [exposureAl, setexposureAl] = useState("");
   const [length, setlength] = useState("13");
@@ -28,6 +28,7 @@ const CalculationsForm2 = () => {
   const [linearidad3, setlinearidad3] = useState("");
   const [linearidad4, setlinearidad4] = useState("");
   const [primaryRay, setprimaryRay] = useState("");
+  const [CoefficientRange, setCoeffiecientRange] = useState("");
   const [less01percent, setless01percent] = useState("");
   const [skinExposure, setskinExposure] = useState("");
   const [dosemGy, setdosemGy] = useState("");
@@ -35,8 +36,10 @@ const CalculationsForm2 = () => {
   useEffect(() => {
     const equationElement1 = document.getElementById("lineality");
     const equationElement2 = document.getElementById("percent");
+    const equationElement3 = document.getElementById("CoeffRange");
     const latexEquation = String.raw`\frac{mR}{mA*t}`;
     const percentEq = String.raw`\text{< } 0.1\%`;
+    const coeffRangeEq = String.raw`\frac{max-min}{max+min} \leq 0.1\%`;
 
     try {
       katex.render(latexEquation, equationElement1, {
@@ -44,6 +47,10 @@ const CalculationsForm2 = () => {
         displayMode: true,
       });
       katex.render(percentEq, equationElement2, {
+        throwOnError: false,
+        displayMode: true,
+      });
+      katex.render(coeffRangeEq, equationElement3, {
         throwOnError: false,
         displayMode: true,
       });
@@ -56,53 +63,72 @@ const CalculationsForm2 = () => {
     event.preventDefault();
 
     // Calcular
+    const linearidadValues = [];
+
     const calclinearidad1 = () => {
       const x = exposure1 / (time1 * mA);
-      setlinearidad1(x);
+      setlinearidad1(x.toFixed(2));
+      linearidadValues.push(x);
     };
     calclinearidad1();
 
     const calclinearidad2 = () => {
       const x = exposure2 / (time2 * mA);
-      setlinearidad2(x);
+      setlinearidad2(x.toFixed(2));
+      linearidadValues.push(x);
     };
     calclinearidad2();
 
     const calclinearidad3 = () => {
       const x = exposure3 / (time3 * mA);
-      setlinearidad3(x);
+      setlinearidad3(x.toFixed(2));
+      linearidadValues.push(x);
     };
     calclinearidad3();
 
     const calclinearidad4 = () => {
       const x = exposure4 / (time4 * mA);
-      setlinearidad4(x);
+      setlinearidad4(x.toFixed(2));
+      linearidadValues.push(x);
     };
     calclinearidad4();
+
+    //Coefficient range
+
+    // Find the maximum and minimum values
+    const calcCoefficientRange = () => {
+      const max = Math.max(...linearidadValues);
+      const min = Math.min(...linearidadValues);
+      const x = (max - min) / (max + min);
+      setCoeffiecientRange(x.toFixed(2));
+    };
+    calcCoefficientRange();
+
+    // Calculate the expression
 
     // Calcular el primaryRay
     const calcprimaryRay = () => {
       const x = (2 * exposure4 * distance ** 2) / distanceSkin ** 2;
-      setprimaryRay(x);
+      setprimaryRay(x.toFixed(2));
     };
     calcprimaryRay();
 
     const calcless01percent = () => {
       const x = (0.1 * primaryRay) / 100;
-      setless01percent(x);
+      setless01percent(x.toFixed(2));
     };
     calcless01percent();
 
     const calchvl = () => {
       const x = (parseFloat(0.693) * 3) / Math.log(exposure4 / exposureAl);
-      sethvl(x);
+      sethvl(x.toFixed(2));
     };
     calchvl();
 
     //Skin exposure
     const calcskinExposure = () => {
       const x = (bitewing * distance ** 2) / distanceSkin ** 2;
-      setskinExposure(x);
+      setskinExposure(x.toFixed(1));
     };
     calcskinExposure();
 
@@ -293,21 +319,25 @@ const CalculationsForm2 = () => {
           </form>
 
           <div id="result1" className="mt-4">
-            <h4 className="text-center">Resultados:</h4>
+            <h4 className="text-center">Results:</h4>
             <div>
               <p id="lineality"></p>
             </div>
-            <p className="text-center">lineality #1: {linearidad1}</p>
-            <p className="text-center">lineality #2: {linearidad2}</p>
-            <p className="text-center">lineality #3: {linearidad3}</p>
-            <p className="text-center">lineality #4: {linearidad4}</p>
-            <p className="text-center">Primary ray: {primaryRay} mR</p>
+            <p className="text-left">lineality #1: {linearidad1}</p>
+            <p className="text-left">lineality #2: {linearidad2}</p>
+            <p className="text-left">lineality #3: {linearidad3}</p>
+            <p className="text-left">lineality #4: {linearidad4}</p>
+            <p className="text-left">Primary ray: {primaryRay} mR</p>
+            <div>
+              <p id="CoeffRange"> </p>
+            </div>
+            <p className="text-left">Coefficient Range: {CoefficientRange}</p>
             <div>
               <p id="percent"></p>
             </div>
-            <p className="text-center"> {less01percent}</p>
-            <p className="text-center">HVL: {hvl} mm Al</p>
-            <p className="text-center">
+            <p className="text-left"> {less01percent}</p>
+            <p className="text-left">HVL: {hvl} mm Al</p>
+            <p className="text-left">
               Skin exposure - bitewing-: {skinExposure}
             </p>
           </div>
